9d89a25f97fb37937d0dd4b25740cd8db2449817
Bruno <bvaleiro@gmail.com>
2024-01-25 23:13:59 -0300

    add banner

---
+++ src/components/Banner/stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Banner from '.'
+const meta: Meta<typeof Banner> = {
+  component: Banner,
+  decorators: [
+    (Story) => (
+      <div style={{ maxWidth: '104rem', margin: '0 auto' }}>
+        <Story />
+      </div>
+    ),
+  ],
+}
+export default meta;
+type Story = StoryObj<typeof Banner>
+export const Default: Story = {
+  args: {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  },
+  parameters: {
+    layout: 'fullscreen',
+
+  },
+}
+
